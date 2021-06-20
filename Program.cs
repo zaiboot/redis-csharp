e71726be85130233e13aec9b1c65ade970f45124
zaiboot <zaiboot@gmail.com>
2021-06-20 13:26:09 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FreeRedis;
+
+namespace redis_csharp
+{
+  public class Program
+  {
+    private const string KEY_NAME = "Test01";
+    public static void Main(string[] args)
+    {
+      using (var cli = new RedisClient("127.0.0.1:6379"))
+      {
+        //cli.Serialize = obj => JsonConvert.SerializeObject(obj);
+        //cli.Deserialize = (json, type) => JsonConvert.DeserializeObject(json, type);
+        cli.Notice += (s, e) => Console.WriteLine(e.Log); //print command log
+
+        cli.RPush(KEY_NAME, "ONE ELEMENT");
+
+        string value1 = cli.RPop(KEY_NAME);
+        Console.WriteLine(value1);
+      }
+
+    }
+  }
+}
